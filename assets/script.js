fc50e89a51667f3c6f7c89d76978410c98eb6aa2
cMoore <celestealexmoore@gmail.com>
2023-02-15 14:22:07 -0600

    stores user choices to variables.

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,4 @@
 /* 
-on page load, set window prompts.
-
-do you want uppercase?
-do you want lowercase?
-do you want numbers?
-do you want special characters?
-
-If user answers yes, store adjacent array to variable.
-
 once all questions have been answered, innerText of box should read
 "click generate password to see your password."
 
@@ -21,14 +12,13 @@ I want to set finalPassword variable = get first (set number of characters) from
 const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-const specialCharacters = ["-", "_", "!", "@", "$", "/","~"];
+const specialChars = ["-", "_", "!", "@", "$", "/","~"];
 let passwordChoices = [];
 let finalPassword;
 
 let charCount = prompt('How many letters would you like your password to contain?', 'Enter a number between 8-128.')
 
 function sanitize(charCount) { 
-
     if(charCount < 8 || charCount > 128 || isNaN(charCount)) {
         let revisedCharCount = prompt('How many letters would you like your password to contain?', 'Enter a number between 8-128.')
         sanitize(revisedCharCount)
@@ -36,3 +26,17 @@ function sanitize(charCount) {
 }
 
 sanitize(charCount)
+
+if(confirm("Do you want your password to contain Capital Letters?")) {
+    passwordChoices = passwordChoices.concat(uppercase);
+    console.log(passwordChoices);
+}if(confirm("Do you want your password to contain Lowercase Letters?")) {
+    passwordChoices = passwordChoices.concat(lowercase);
+    console.log(passwordChoices);
+}if(confirm("Do you want your password to contain Numbers?")) {
+    passwordChoices = passwordChoices.concat(numbers);
+    console.log(passwordChoices);
+}if(confirm("Do you want your password to contain Special Characters?")) {
+    passwordChoices = passwordChoices.concat(specialChars);
+    console.log(passwordChoices);
+}
